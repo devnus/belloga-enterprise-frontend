a611254e8eef3fb528e156a6c64362408aba4ef2
seolyeongbae <peixueying@gmail.com>
2022-07-27 22:28:47 -0700

    fix: type error resolved

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { applyMiddleware, createStore } from "redux";
 import rootReducer, { rootSaga } from "./modules";
 import { Provider } from "react-redux";
@@ -31,8 +30,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
