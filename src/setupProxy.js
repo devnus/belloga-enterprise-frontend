48ebbafdfaf85bd9a0b2365676db47d8b9658830
seolyeongbae <peixueying@gmail.com>
2022-10-11 16:38:21 +0900

    fix: setup env files

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/api",
+    `${process.env.REACT_APP_API_URL}/api`,
     createProxyMiddleware({
       target: process.env.REACT_APP_API_URL,
       changeOrigin: true,
