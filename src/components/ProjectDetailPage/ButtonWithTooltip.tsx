cd1fd3be58ac55b903e19ca2a32037c4f76afa2a
seolyeongbae <peixueying@gmail.com>
2022-11-10 21:27:39 +0900

    feat: after press copy btn let tooltip appear

--- src/components/ProjectDetailPage/ButtonWithTooltip.tsx
+++ src/components/ProjectDetailPage/ButtonWithTooltip.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const BUTTONSTYLE =
   "inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-mainBlue hover:bg-mainLightBlue";
@@ -14,22 +14,46 @@ const ButtonWithTooltip = ({
   children,
   tooltipDescription,
 }: buttonWithTooltip) => {
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+
+  const onClick = () => {
+    onClickFunction();
+    setIsTooltipVisible(() => true);
+  };
+
+  //transision을 줘서 버튼을 누른 다음에 작은 툴팁이 뜨게 만든다, 툴팁은 1초 뒤에 사라진다.
+  useEffect(() => {
+    if (isTooltipVisible === true) {
+      setTimeout(() => {
+        setIsTooltipVisible(() => false);
+      }, 1000);
+    }
+  }, [isTooltipVisible]);
+
   return (
-    <div className="group cursor-pointer relative inline-block text-center">
-      <button type="button" onClick={onClickFunction} className={BUTTONSTYLE}>
+    <div className="group cursor-pointer relative inline-block text-center px-2">
+      <button type="button" onClick={onClick} className={BUTTONSTYLE}>
         {children}
-        <div className="w-full opacity-0 bg-black text-white text-center text-mds rounded-lg py-2 absolute z-10 group-hover:opacity-100 bottom-full px-3 pointer-events-none">
-          {tooltipDescription}
-          <svg
-            className="absolute text-black h-2 w-full left-0 top-full"
-            x="0px"
-            y="0px"
-            viewBox="0 0 255 255"
-          >
-            <polygon className="fill-current" points="0,0 127.5,127.5 255,0" />
-          </svg>
-        </div>
       </button>
+
+      <div
+        className={
+          (isTooltipVisible
+            ? "transition-opacity duration-1000 ease-out opacity-100 "
+            : "opacity-0 ") +
+          "absolute w-full bg-black text-white text-center text-mds rounded-lg py-2 z-10 group-focus:opacity-100 bottom-full pointer-events-none"
+        }
+      >
+        {tooltipDescription}
+        <svg
+          className="absolute text-black h-2 w-full left-0 top-full"
+          x="0px"
+          y="0px"
+          viewBox="0 0 255 255"
+        >
+          <polygon className="fill-current" points="0,0 127.5,127.5 255,0" />
+        </svg>
+      </div>
     </div>
   );
 };
